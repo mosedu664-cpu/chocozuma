d2873f8fab11f4bfbe111d177dd3b449f1d5b6aa
ahmedkhak <ahmed.khakwani@yahoo.com>
2026-04-02 20:36:46 +0500

    Prevent Payment Without Sending Order to Kitchen (KDS Validation)

--- cz_customizations/static/src/js/Store/pos_store.js
+++ cz_customizations/static/src/js/Store/pos_store.js
@@ -1,12 +1,52 @@
+/** @odoo-module */
+
 import { PosStore } from "@point_of_sale/app/store/pos_store";
 import { patch } from "@web/core/utils/patch";
+import { _t } from "@web/core/l10n/translation";
+import { AlertDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
 
 patch(PosStore.prototype, {
+    /**
+     * Override pay() to block payment when the order has unsent kitchen changes.
+     * In restaurant mode, the order must first be sent to the kitchen via
+     * the "Order" button before the cashier can proceed to payment.
+     */
     async pay() {
-        let order = this.env.services.pos.get_order();
-        var error = false;
-        if(!error){
-            super.pay();
+        if (this.config.module_pos_restaurant) {
+            const currentOrder = this.get_order();
+            if (currentOrder && !currentOrder.finalized) {
+                const changes = this.getOrderChanges();
+                const hasUnsent =
+                    changes.nbrOfChanges > 0 ||
+                    changes.generalNote !== undefined ||
+                    changes.modeUpdate;
+
+                if (hasUnsent) {
+                    this.dialog.add(AlertDialog, {
+                        title: _t("Order Not Sent to Kitchen"),
+                        body: _t("You must send the order to the kitchen before proceeding to payment. Please click the 'Order' button first."),
+                    });
+                    return;
+                }
+
+                // Also block if the order has never been sent to kitchen at all
+                // (brand new order with lines but last_order_preparation_change is empty)
+                const lastChange = currentOrder.last_order_preparation_change;
+                if (
+                    currentOrder.lines.length > 0 &&
+                    Object.keys(lastChange.lines).length === 0 &&
+                    !lastChange.metadata?.serverDate
+                ) {
+                    this.dialog.add(AlertDialog, {
+                        title: _t("Order Not Sent to Kitchen"),
+                        body: _t(
+                            "You must send the order to the kitchen before proceeding to payment. Please click the 'Order' button first."
+                        ),
+                    });
+                    return;
+                }
+            }
         }
-    }
+        return super.pay();
+    },
 });
